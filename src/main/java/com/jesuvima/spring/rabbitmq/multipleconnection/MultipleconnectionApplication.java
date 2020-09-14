47f3d8e94cba8c902d72cd190f27213bc74e48c6
jesusvillalbam@gmail.com <jevillalba@personalsoft.com.co>
2020-09-14 17:27:09 -0500

    Start

---
+++ src/main/java/com/jesuvima/spring/rabbitmq/multipleconnection/MultipleconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.jesuvima.spring.rabbitmq.multipleconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleconnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesuvima/spring/rabbitmq/multipleconnection/MultipleconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesuvima.spring.rabbitmq.multipleconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
